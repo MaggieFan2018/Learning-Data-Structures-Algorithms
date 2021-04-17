f84da4b90c1326aa69134d6ffbc2f3a127854442
Mengqi Fan <fanm3@rpi.edu>
2021-04-17 13:16:31 -0400

    Update 5. Duplicate Number.py

--- Learning Data Structures/Arrays and Linked Lists/5. Duplicate Number.py
+++ Learning Data Structures/Arrays and Linked Lists/5. Duplicate Number.py
@@ -1,1 +1,17 @@
+#Problem Statement
+#- You have been given an array of length = n. The array contains integers from 0 to n - 2. 
+#- Each number in the array is present exactly once except for one number which is present twice. 
+#- Find and return this duplicate number present in the array
 
+
+def duplicate_number(arr):
+    """
+    :param - array containing numbers in the range [0, len(arr) - 2]
+    return - the number that is duplicate in the arr
+    """
+    new_list = []
+    for element in arr:
+        if element not in new_list:
+            new_list.append(element)
+        else:
+            return element

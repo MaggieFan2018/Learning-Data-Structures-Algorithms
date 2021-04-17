23eeeb970be78bb0fd6ea06a01e2f814af2660a6
Mengqi Fan <fanm3@rpi.edu>
2021-04-17 13:05:34 -0400

    Update 3. Detecting Loops in Linked Lists.py

--- Learning Data Structures/Arrays and Linked Lists/3. Detecting Loops in Linked Lists.py
+++ Learning Data Structures/Arrays and Linked Lists/3. Detecting Loops in Linked Lists.py
@@ -1,1 +1,69 @@
+#Task: implement a function that detects if a loop exists in a linked list.
 
+#Helper Code
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+class LinkedList:
+    def __init__(self, init_list=None):
+        self.head = None
+        if init_list:
+            for value in init_list:
+                self.append(value)
+        
+    def append(self, value):
+        if self.head is None:
+            self.head = Node(value)
+            return
+        
+        # Move to the tail (the last node)
+        node = self.head
+        while node.next:
+            node = node.next
+        
+        node.next = Node(value)
+        return
+      
+      
+list_with_loop = LinkedList([2, -1, 3, 0, 5])
+
+# Creating a loop where the last node points back to the second node
+loop_start = list_with_loop.head.next
+
+node = list_with_loop.head
+while node.next: 
+    node = node.next   
+node.next = loop_start
+
+
+#---------------------------- Write the function definition ---------------------------------#
+def iscircular(linked_list):
+    """
+    Determine whether the Linked List is circular or not
+
+    Args:
+       linked_list(obj): Linked List to be checked
+    Returns:
+       bool: Return True if the linked list is circular, return False otherwise
+    """
+    
+    # TODO: Write function to check if linked list is circular
+    if linked_list.head == None:
+        return False
+    
+    else:
+        slow = linked_list.head
+        fast = linked_list.head
+        
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            
+            if slow == fast:
+                return True
+            
+        return False
+    #---------------------------------------------------------------------------------------#
